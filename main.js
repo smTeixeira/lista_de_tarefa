ce178a41fb3c0066469adadcb22a033e5211b179
samuel de Matos Teixeira <samuelteixeira00@hotmail.com>
2023-06-22 21:14:59 -0300

    Modificação do codigo para ajustar o erro de conclusão de tarefas

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ $(document).ready(function() {
     $('form').on('submit', function(e)  {
         e.preventDefault();
         const endereçoDaNovaTarefa = $('#endereço-tarefa').val();
-        const novaTarefa = $('<li></li>');
+        const novaTarefa = $('<li class="nova-tarefa"></li>');
 
         $(`
-        <li> ${endereçoDaNovaTarefa}</li>
+        <div>${endereçoDaNovaTarefa}</div>f
+
         `).appendTo(novaTarefa);
         $(novaTarefa).appendTo('ul');
         $('#endereço-tarefa').val('')
     })
 
-    $('ul').click(function(){
-        $('li').addClass('concluido');
+    $('ul').on('click','li',function(){
+        $(this).css("text-decoration", "line-through");
     })
 })
